87da5bc292745a4cc7f2e5bf0595b8f16e246430
liqx <s.vlasiuk@dev-branch.com>
2024-10-04 08:03:57 +0000

    Added custom modules

--- web/drush-archive/code/web/sites/default/memcache.php
+++
@@ -1,5 +0,0 @@
-<?php
-$settings['cache']['default'] = 'cache.backend.memcache';
-$settings['memcache']['servers'] = ['memcached:11211' => 'default'];
-$settings['memcache']['bins'] = ['default' => 'default'];
-$settings['memcache']['key_prefix'] = 'mysite';
