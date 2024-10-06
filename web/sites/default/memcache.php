9ecdbdc381b0c592ef0a1cacda78908ba117de1d
liqx <s.vlasiuk@dev-branch.com>
2024-10-06 10:19:56 +0000

    sdsad

--- web/sites/default/memcache.php
+++ web/sites/default/memcache.php
@@ -1,4 +1,4 @@
-< ?php
+sd< ?php
 $settings['cache']['default'] = 'cache.backend.memcache';
 $settings['memcache']['servers'] = ['memcached:11211' => 'default'];
 $settings['memcache']['bins'] = ['default' => 'default'];
