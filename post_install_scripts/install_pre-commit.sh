337082cde13c5b01501d5d3ef905b248b4fc1c6a
liqx <s.vlasiuk@dev-branch.com>
2024-10-10 07:45:12 +0000

    test

--- post_install_scripts/install_pre-commit.sh
+++ post_install_scripts/install_pre-commit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-cp ./.githooks/pre-commit ./.git/hooks/pre-commit
-chmod +x ./.git/hooks/pre-commit
-npm install eslint eslint-config-airbnb eslint-plugin-yml --save-dev
-npm i eslint-config-drupal
-echo "The pre-commit hook installed"
+if [[ -n "$PANTHEON_ENVIRONMENT" ]]; then
+    echo "You try to set a pre-commit hook on the Pantheon server. Skipping!"
+else
+    cp ./.githooks/pre-commit ./.git/hooks/pre-commit
+    chmod +x ./.git/hooks/pre-commit
+    npm install eslint eslint-config-airbnb eslint-plugin-yml --save-dev
+    npm i eslint-config-drupal
+    echo "The pre-commit hook installed"
+fi
