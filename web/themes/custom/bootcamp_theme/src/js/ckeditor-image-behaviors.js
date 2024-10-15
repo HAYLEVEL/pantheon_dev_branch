1b62743d4fd8cccedefb99dc0cc13c67161bd108
liqx <s.vlasiuk@dev-branch.com>
2024-10-15 09:33:40 +0000

    test

--- web/themes/custom/bootcamp_theme/src/js/ckeditor-image-behaviors.js
+++ web/themes/custom/bootcamp_theme/src/js/ckeditor-image-behaviors.js
@@ -18,7 +18,7 @@
 	// sdasdasd
         return;
       }
-      let sliderElements = document.querySelectorAll('.paragraph--type--ckeditor-slides .swiffy-slider:not(.swiffy-slider__gallery-initialization-container)');
+      const sliderElements = document.querySelectorAll('.paragraph--type--ckeditor-slides .swiffy-slider:not(.swiffy-slider__gallery-initialization-container)');
       Array.from(sliderElements).forEach(function (sliderElement) {
         swiffyslider.onSlideEnd(sliderElement, function () {
           const currentActiveSlide = sliderElement.querySelector('.active');
